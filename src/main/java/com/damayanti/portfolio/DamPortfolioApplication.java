8cbba6263560f2e8992830b3877028d5eef5a256
Damayanti Chavan <damayantichavan83518@gmail.com>
2025-08-16 14:40:37 +0530

    Initial commit from Eclipse project

---
+++ src/main/java/com/damayanti/portfolio/DamPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.damayanti.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamPortfolioApplication.class, args);
+	}
+
+}
